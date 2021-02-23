72dfb50ceb1aaa5c15ae56fa1699ea02a28ad61c
Karmello101 <kevinarmella101@gmail.com>
2021-02-22 21:03:59 -0500

    added props to product component no css edits

--- src/Home.js
+++ src/Home.js
@@ -12,13 +12,46 @@ function Home() {
                 alt="Prime"
                 />
                 <div className="home__row">
-                 <Product />
+                 <Product  
+                  title="Rich Dad Poor Dad"
+                  price={11.77}
+                  image="https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg" alt=" Rich Dad Poor Dad"
+                  rating={5}
+                  />
+                 <Product 
+                  title="Nintendo Switch with Neon Blue and Neon red Joy-Con"
+                  price={299.00}
+                  image="https://images-na.ssl-images-amazon.com/images/I/61-PblYntsL._AC_UL200_SR200,200_.jpg"
+                  rating={5}
+                  />
                 </div>
                 <div className="home__row">
-                    
+                  <Product
+                   title="Apple Watch Series 3 (GPS, 38mm) - Space Gray Aluminum Case"
+                   price={169.00}
+                   image="https://images-na.ssl-images-amazon.com/images/I/71fwbMm1NBL._AC_UL200_SR200,200_.jpg"
+                   rating={5} 
+                   />
+                  <Product 
+                  title="Apple MacBook Pro with Apple M1 Chip - Space Gray"
+                  price={1219.00}
+                  image="https://m.media-amazon.com/images/I/71an9eiBxpL._AC_UY218_.jpg"
+                  rating={5}
+                  />
+                  <Product 
+                  title="Insignia NS-32DF310NA19 32-inch Smart HD TV"
+                  price={129.99}
+                  image="https://m.media-amazon.com/images/I/41BS3eLBuPL._AC_UY218_.jpg"
+                  rating={4}
+                  />
                 </div>
                 <div className="home__row">
-                    
+                <Product 
+                title="Black Decker 20V MAX Drill & Home Tool Kit, 68 Piece"
+                price={74.99}
+                image="https://m.media-amazon.com/images/I/61yrn8PjUML._AC_UL320_.jpg"
+                rating={4}
+                />
                 </div>
             </div>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import "./Product.css";
-function Product() {
+
+function Product({ title , image , price , rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>Rich Dad Poor Dad</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>11.77</strong>
+                    <strong>{price}</strong>
                 </p>
                <div className="product__rating">
-
+                 {Array(rating)
+                 .fill()
+                 .map((_, i) => (
+                    <p>⭐️</p>
+                 ))}
+                 
                </div>
 
             </div>
-            <img src="https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL._AC_UL200_SR200,200_.jpg" alt=" Rich Dad Poor Dad"/>
+            <img src={image} alt=""/>
             <button>Add to Cart</button>
         </div>
     )
