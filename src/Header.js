3e96ba426d81a8ce50d73e401309369d167e35d1
Karmello101 <kevinarmella101@gmail.com>
2021-02-24 19:27:27 -0500

    complete most of the reducer and stateprovider components for cart functionality

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+
+    const [state, dispatch] = useStateValue();
     return (
         <div className='header'>
+            <Link to="/">
             <img 
               className="header__logo" 
               src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            </Link>
             
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
@@ -27,13 +33,15 @@ function Header() {
                 <span className="header__optionLineOne">Your</span>
                 <span className="header__optionLineTwo">Prime</span>
                 </div>
-                <div className="header__optionCart">
-                 <ShoppingCartIcon />
-                 <span className="header__optionLineTwo header__cartCount">0</span>
-                </div>
-            </div>
-        </div>
 
+                <Link to="/Checkout">
+                 <div className="header__optionCart">
+                  <ShoppingCartIcon />
+                  <span className="header__optionLineTwo header__cartCount">0</span>
+                 </div>
+                </Link>
+         </div>
+        </div>
     )
 }
 
