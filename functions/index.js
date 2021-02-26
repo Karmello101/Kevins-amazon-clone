b8fbfd0003fdf0fe4473fec6b75048f3393a39cb
Karmello101 <kevinarmella101@gmail.com>
2021-02-26 17:14:36 -0500

    added checkout screen and order list that deployed app doesn't have

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,9 @@
 const functions = require("firebase-functions");
-const express = require("express");
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const admin = require('firebase-admin');
-admin.initializeApp();
 
-const db = admin.firestore();
-const stripe = require("stripe")
-("sk_test_51IOuwHHvRnygUna7pOpT72giyvZDE7Q7Tto9JSpG86TAihwvdQKF27M1RHFzAdLCc0N42PwI4gCz6rJVtIkYjjs800ZBSqiPBl");
-
-
-app.use(cors({}));
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}))
-
-//app.get('/', (request, response) => response.status(200).send("hello world"));
-app.get('/', async(req,res) => {
-    const intent = paymentIntent;
-    res.json({client_secret: intent.client_secret});
-});
-
-app.post('/payment', async (req, res) => {
-    const paymentIntent = await stripe.paymentIntent.create({
-        amount: 5000,
-        currency: 'usd',
-        metadata: {integration_check: 'accept_a_payment'}
-    });
-    res.json({'client_secret': paymentIntent('client_secret')})
-})
-
-//app.post("/payment/create", async (request, response) => {
-    //const total = request.query.total;
-
-    //console.log('Payment Request Recieved', total);
-    
-    //const paymentIntent = await stripe.paymentIntent.create({
-       // amount: total,
-       // currency: "usd",
-       // metadata: {integration_check: 'accept_a_payment'}
-   // });
-
-    //response.status(201).send({
-        //clientSecret: paymentIntent.client_secret,
-    //});
-//});
-
-app.listen(port, () => console.log(`$port`))
-//exports.api = functions.http.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDNuxOwUkr1Tfma0CRhJzWNI4cHsGPU9us",
-  authDomain: "kevins--clone.firebaseapp.com",
-  projectId: "kevins--clone",
-  storageBucket: "kevins--clone.appspot.com",
-  messagingSenderId: "238830002889",
-  appId: "1:238830002889:web:30c98bed4006273441a318",
-  measurementId: "G-CDSSV11GRQ"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDNuxOwUkr1Tfma0CRhJzWNI4cHsGPU9us",
+    authDomain: "kevins--clone.firebaseapp.com",
+    projectId: "kevins--clone",
+    storageBucket: "kevins--clone.appspot.com",
+    messagingSenderId: "238830002889",
+    appId: "1:238830002889:web:f6720988710b6cca41a318",
+    measurementId: "G-828FJ3L476"
+  };
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
